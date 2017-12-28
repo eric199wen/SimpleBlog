4ea9a0d2d982ff8ded65748b565319715fe9f519
eric199wen <eric199.wen@gmail.com>
2017-12-28 10:02:02 +0800

    can fetch posts using api

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// BrowserRouter interact with History library
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
+import PostsNew from './components/PostsNew';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        {/* if the user go to path, then render component */}
+        {/* react route use loose match, so we use Switch component */}
+        {/* match the first route match the url */}
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
