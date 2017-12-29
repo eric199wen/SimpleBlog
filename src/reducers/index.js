eb189bea13a2c09f2063cf020c181e4f9e081fde
eric199wen <eric199.wen@gmail.com>
2017-12-29 08:07:44 +0800

    get user input when pressing submit button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+// renaming the property reducer to formReducer
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from "./ReducerPosts";
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer // key has to be "form"
 });
 
 export default rootReducer;
