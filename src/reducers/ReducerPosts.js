7dd114fe71ff9e88476bee065e307a058726b2e3
eric199wen <eric199.wen@gmail.com>
2017-12-29 10:17:32 +0800

    delete a post

--- src/reducers/ReducerPosts.js
+++ src/reducers/ReducerPosts.js
@@ -1,8 +1,11 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      //return a new state object without the particular post 
+      return _.omit(state, action.payload);
     case FETCH_POST:
       // ES5 syntax
       // const post = action.payload.data;
